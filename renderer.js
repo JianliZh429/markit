beb49a9b6abeb6e61ba78b1dcf7d28bb71898a53
Jianli Zhang <jianli.zh42g@gmail.com>
2024-02-21 23:14:17 +1100

    first try

---
+++ renderer.js
@@ -0,0 +1,10 @@
+const marked = require("marked");
+
+const markdownInput = document.getElementById("markdown-input");
+const previewPane = document.getElementById("preview-pane");
+
+markdownInput.addEventListener("input", () => {
+  const markdownContent = markdownInput.value;
+  const htmlContent = marked(markdownContent);
+  previewPane.innerHTML = htmlContent;
+});
