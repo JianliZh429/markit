e8c497a52acc37413d3aa061a35da1c4267343ef
Jianli Zhang <jianli.zh42g@gmail.com>
2024-02-22 22:36:01 +1100

    make it working

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 960,
+    height: 720,
     webPreferences: {
+      enableRemoteModule: true,
+      contextIsolation: false,
       nodeIntegration: true,
     },
   });
--- renderer.js
+++ renderer.js
@@ -1,4 +1,4 @@
-const marked = require("marked");
+const { marked } = require("marked");
 
 const markdownInput = document.getElementById("markdown-input");
 const previewPane = document.getElementById("preview-pane");
